149cb490101e5fc6a807e0bb43fccc97e9b372ec
ChrisCaineUoN <egycc7@nottingham.ac.uk>
2023-10-03 12:19:11 +0100

    Second edit

--- Chapter 4/4.3/4.3.1/integer.c
+++ Chapter 4/4.3/4.3.1/integer.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main(void)
 {
-int Age=0, Yearofbirth=1989;
+int Age=0, Yearofbirth=2000;
 Age = 2023-Yearofbirth;
-printf("Your age is %d;
+printf("Your age is %d", Age);
 }
